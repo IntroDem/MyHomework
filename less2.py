7ad8de83b7c2d40240457c535421d56bd441053f
IntroDem <99888313+IntroDem@users.noreply.github.com>
2024-06-29 13:10:23 +0300

    Урок 2. Математические операции

---
+++ less2.py
@@ -0,0 +1,26 @@
+# input_valute = input("Введите сумму: ")
+dollar_cource = 80.2
+
+# print(int(input_valute) / dollar_cource)
+print(2**3)  # Возведение в стпень
+
+print(10 % 3)  # Остаток от деления
+
+
+hello = "Hello World"
+print(hello)
+hello = "Hello"
+print(hello)
+
+var_1 = 1
+print(var_1)
+var_2 = var_1 + 1
+print(var_2)
+var_2 += 1
+print(var_2)
+var_2 *= 2
+var_2 /= 2
+var_2 %= 2  # Остаток от деления
+var_2 **= 2  # Степень  ываываыва
+var_2 //= 2  # Деление без остатка фывфывasdasdasdsваав
+var_2 -= 2
